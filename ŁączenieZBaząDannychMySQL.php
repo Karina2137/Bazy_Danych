010027c549f922d78bd981f4ed2f94a1ae13732c
Karina Ponomarova <114558445+Karina2137@users.noreply.github.com>
2023-10-10 09:18:40 +0200

    Update ŁączenieZBaząDannychMySQL.php

--- ŁączenieZBaząDannychMySQL.php
+++ ŁączenieZBaząDannychMySQL.php
@@ -24,3 +24,30 @@ $con->close();
 
 //znacznik końca pliku PHP
 ?>
+
+<?PHP
+//phpinfo();
+
+//znak konkatynacji(złączenia) - .
+echo "Klasa 2E Łączenie z bazą dannych MySQL"."<br>","<br>";
+//$nazwa - tworzenie zmiennej
+$serwer = "localhost";
+$uzytkownik = "root";
+$haslo = "";
+$baza = "AgencjaArtystycznaPrzyklad";
+$con= new mysqli($serwer, $uzytkownik, $haslo, $baza);
+
+//albo
+//$con= new mysqli("127.0.0.1:3306","root","","Wykonawcy");
+$zapytanie="SELECT NazwaScenicznaWykonawcy FROM Wykonawcy ORDER BY NazwaScenicznaWykonawcy";
+if($wynik=$con->query($zapytanie))
+//fetch_array - tablica skojarzona z bazy danych
+ WHILE($row=$wynik->fetch_array())
+  echo $row["NazwaScenicznaWykonawcy"]."<br>";
+else
+ echo $con->error." ".$con->error;
+//zamknięcie połączenia z bazą
+$con->close();
+
+//znacznik końca pliku PHP
+?>
